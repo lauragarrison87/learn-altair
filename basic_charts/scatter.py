62f39ea1b83b1be1ba711e5d4e284a834098e801
Laura <lauragarrison87@gmail.com>
2022-11-21 08:35:34 +0100

    adding plots

--- basic_charts/scatter.py
+++ basic_charts/scatter.py
@@ -1,33 +1,31 @@
 import altair as alt
-from vega_datasets import (
-    data,
-)  # from https://github.com/vega/vega-datasets/tree/master/data
+from vega_datasets import data
+# from https://github.com/vega/vega-datasets/tree/master/data
 
 source = data.gapminder_health_income()
 print(source.head())
 
 # plot health against income for all countries
-scatter_plot_linear = (
-    alt.Chart(source)
-    .mark_point()
-    .encode(
+scatter_plot_linear_zero = alt.Chart(source).mark_circle().encode(
         alt.X("income:Q"),
-        alt.Y(
-            "health:Q", scale=alt.Scale(zero=False)
-        ),  # what happens if this is set to true?
+        alt.Y("health:Q"),
     )
-)
 
-scatter_plot_log = (
-    alt.Chart(source)
-    .mark_circle()
-    .encode(
-        alt.X("income:Q", scale=alt.Scale(type="log")),
+# truncate y axis for more spread out representation
+scatter_plot_linear = alt.Chart(source).mark_circle().encode(
+        alt.X("income:Q"),
         alt.Y("health:Q", scale=alt.Scale(zero=False)),
-    )
-)
+    ).interactive()
 
-scatter_plot_linear.show()  # to display in altair viewer (must be installed separately)
-scatter_plot_log.show()  # log spreads things out a bit more
+# logarithmic scale to spread out poorer countries and encode population into visualization 
+scatter_plot_log = alt.Chart(source).mark_circle().encode(
+        alt.X("income:Q", scale=alt.Scale(type="log"), title='Income'),
+        alt.Y("health:Q", scale=alt.Scale(zero=False), title='Health'),
+        # size='population:Q'
+    ).interactive()
 
-# chart.save('basic-chart.html')
+alt.vconcat(
+    # scatter_plot_linear_zero, 
+    scatter_plot_linear.encode(color='population:O'), 
+    scatter_plot_log.encode(size='population:Q', color='population:Q')
+    ).save("scatter-charts.html")
